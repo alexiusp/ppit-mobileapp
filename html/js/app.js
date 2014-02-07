f6add7c1c479d42be1f36f8ec75853c7cc6894cf
AlexiuS Podgaev <aleksei.podgaev@people-projects-it.com>
2014-02-07 10:20:53 +0100

    debug 5 - prod version

--- html/js/app.js
+++ html/js/app.js
@@ -4,9 +4,9 @@ var _VERSION = 458;
 //var _PLATFORM = "debug";
 var _PLATFORM = "android";
 //var _PLATFORM = "ios";
-//var _URL = "https://m.people-projects-it.com";
+var _URL = "https://m.people-projects-it.com";
 //var _URL = "https://m-proxy.people-projects-it.com";
-var _URL = "https://10.21.0.11";
+//var _URL = "https://10.21.0.11";
 /**
  * german localization
  */
