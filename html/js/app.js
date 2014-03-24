c6eb5431d2a580d93e991d478cea2ce8374e5bc6
AlexiuS Podgaev <aleksei.podgaev@people-projects-it.com>
2014-03-24 18:13:06 +0100

    dev version

--- html/js/app.js
+++ html/js/app.js
@@ -2,10 +2,10 @@
 // version 1.3 code 460
 var _VERSION = 460;
 //var _PLATFORM = "debug";
-//var _PLATFORM = "android";
-var _PLATFORM = "ios";
-var _URL = "https://m.people-projects-it.com";
-//var _URL = "https://m.ber.menuplus.de";
+var _PLATFORM = "android";
+//var _PLATFORM = "ios";
+//var _URL = "https://m.people-projects-it.com";
+var _URL = "https://m.ber.menuplus.de";
 //var _URL = "https://m-proxy.people-projects-it.com";
 //var _URL = "https://10.21.0.11";
 /**
